8f705d110d6f2a26df41412cd8fbd9cfeff10e27
Kanri <yiwen_chan@foxmail.com>
2022-01-20 18:10:20 +0800

    🎨 整理代码

--- main.go
+++ main.go
@@ -23,42 +23,28 @@ const (
 	SECTKN = 0x11dc // Parameter (Security Token)
 )
 
-var ebcdic = [256]byte{
-	0, 1, 2, 3, 55, 45, 46, 47, 22, 5, 37, 11, 12, 13, 14, 15,
-	16, 17, 18, 19, 60, 61, 50, 38, 24, 25, 63, 39, 28, 29, 30, 31,
-	64, 79, 127, 123, 91, 108, 80, 125, 77, 93, 92, 78, 107, 96, 75, 97,
-	240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 122, 94, 76, 126, 110, 111,
-	124, 193, 194, 195, 196, 197, 198, 199, 200, 201, 209, 210, 211, 212, 213, 214,
-	215, 216, 217, 226, 227, 228, 229, 230, 231, 232, 233, 74, 224, 90, 95, 109,
-	121, 129, 130, 131, 132, 133, 134, 135, 136, 137, 145, 146, 147, 148, 149, 150,
-	151, 152, 153, 162, 163, 164, 165, 166, 167, 168, 169, 192, 106, 208, 161, 7,
-	32, 33, 34, 35, 36, 21, 6, 23, 40, 41, 42, 43, 44, 9, 10, 27,
-	48, 49, 26, 51, 52, 53, 54, 8, 56, 57, 58, 59, 4, 20, 62, 225,
-	65, 66, 67, 68, 69, 70, 71, 72, 73, 81, 82, 83, 84, 85, 86, 87,
-	88, 89, 98, 99, 100, 101, 102, 103, 104, 105, 112, 113, 114, 115, 116, 117,
-	118, 119, 120, 128, 138, 139, 140, 141, 142, 143, 144, 154, 155, 156, 157, 158,
-	159, 160, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183,
-	184, 185, 186, 187, 188, 189, 190, 191, 202, 203, 204, 205, 206, 207, 218, 219,
-	220, 221, 222, 223, 234, 235, 236, 237, 238, 239, 250, 251, 252, 253, 254, 255,
+type Conn struct {
+	conn net.Conn
 }
 
-var ascii = [256]byte{
-	0, 1, 2, 3, 156, 9, 134, 127, 151, 141, 142, 11, 12, 13, 14, 15,
-	16, 17, 18, 19, 157, 133, 8, 135, 24, 25, 146, 143, 28, 29, 30, 31,
-	128, 129, 130, 131, 132, 10, 23, 27, 136, 137, 138, 139, 140, 5, 6, 7,
-	144, 145, 22, 147, 148, 149, 150, 4, 152, 153, 154, 155, 20, 21, 158, 26,
-	32, 160, 161, 162, 163, 164, 165, 166, 167, 168, 91, 46, 60, 40, 43, 33,
-	38, 169, 170, 171, 172, 173, 174, 175, 176, 177, 93, 36, 42, 41, 59, 94,
-	45, 47, 178, 179, 180, 181, 182, 183, 184, 185, 124, 44, 37, 95, 62, 63,
-	186, 187, 188, 189, 190, 191, 192, 193, 194, 96, 58, 35, 64, 39, 61, 34,
-	195, 97, 98, 99, 100, 101, 102, 103, 104, 105, 196, 197, 198, 199, 200, 201,
-	202, 106, 107, 108, 109, 110, 111, 112, 113, 114, 203, 204, 205, 206, 207, 208,
-	209, 126, 115, 116, 117, 118, 119, 120, 121, 122, 210, 211, 212, 213, 214, 215,
-	216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231,
-	123, 65, 66, 67, 68, 69, 70, 71, 72, 73, 232, 233, 234, 235, 236, 237,
-	125, 74, 75, 76, 77, 78, 79, 80, 81, 82, 238, 239, 240, 241, 242, 243,
-	92, 159, 83, 84, 85, 86, 87, 88, 89, 90, 244, 245, 246, 247, 248, 249,
-	48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 250, 251, 252, 253, 254, 255,
+type DRDA struct {
+	DDM        *DDM
+	Parameters []*Parameter
+}
+
+type DDM struct {
+	Length    int32
+	Magic     byte
+	Format    byte
+	CorrelId  int32
+	Length2   int32
+	CodePoint int32
+}
+
+type Parameter struct {
+	Length    int32
+	CodePoint int32
+	Payload   []byte
 }
 
 func main() {
@@ -169,26 +155,6 @@ func main() {
 	// ))
 }
 
-func ToEBCDIC(data []byte) []byte {
-	var buf = bytes.NewBuffer(make([]byte, 0, len(data)))
-	for i := range data {
-		buf.WriteByte(ebcdic[data[i]])
-	}
-	return buf.Bytes()
-}
-
-func toAscii(data []byte) []byte {
-	var buf = bytes.NewBuffer(make([]byte, 0, len(data)))
-	for i := range data {
-		buf.WriteByte(ascii[data[i]])
-	}
-	return buf.Bytes()
-}
-
-type Conn struct {
-	conn net.Conn
-}
-
 func (conn *Conn) Write(drda *DRDA) error {
 	var dbuf = bytes.NewBuffer(make([]byte, 0))
 	dbuf.WriteByte(0x00)
@@ -227,7 +193,7 @@ func (conn *Conn) Write(drda *DRDA) error {
 func (conn *Conn) Read() (*DRDA, error) {
 	var b1 = make([]byte, 2)
 	conn.conn.Read(b1)
-	var length = bytesToInt32(b1...)
+	var length = (int32(b1[0]) << 8) | int32(b1[1])
 	var b2 = make([]byte, length-2)
 	n, err := conn.conn.Read(b2)
 	if err != nil {
@@ -236,24 +202,24 @@ func (conn *Conn) Read() (*DRDA, error) {
 	if n != int(length-2) {
 		return &DRDA{}, errors.New("Read DRDA payload not enough length")
 	}
-	var ddm = DDM{
+	var ddm = &DDM{
 		Length:    length,
 		Magic:     b2[0],
 		Format:    b2[1],
-		CorrelId:  bytesToInt32(b2[2], b2[3]),
-		Length2:   bytesToInt32(b2[4], b2[5]),
-		CodePoint: bytesToInt32(b2[5], b2[6]),
+		CorrelId:  (int32(b2[2]) << 8) | int32(b2[3]),
+		Length2:   (int32(b2[4]) << 8) | int32(b2[5]),
+		CodePoint: (int32(b2[6]) << 8) | int32(b2[7]),
 	}
 	var drda = DRDA{
-		DDM:        &ddm,
+		DDM:        ddm,
 		Parameters: make([]*Parameter, 0),
 	}
 	for i := 8; i < len(b2); {
-		var pl = bytesToInt32(b2[i], b2[i+1])
+		var pl = (int32(b2[i]) << 8) | int32(b2[i+1])
 		drda.Parameters = append(drda.Parameters,
 			&Parameter{
 				Length:    pl,
-				CodePoint: bytesToInt32(b2[i+2], b2[i+3]),
+				CodePoint: (int32(b2[i+2]) << 8) | int32(b2[i+3]),
 				Payload:   b2[i+4 : i+int(pl)],
 			},
 		)
@@ -262,52 +228,86 @@ func (conn *Conn) Read() (*DRDA, error) {
 	return &drda, nil
 }
 
-type DRDA struct {
-	DDM        *DDM
-	Parameters []*Parameter
+var ebcdic = [256]byte{
+	0, 1, 2, 3, 55, 45, 46, 47, 22, 5, 37, 11, 12, 13, 14, 15,
+	16, 17, 18, 19, 60, 61, 50, 38, 24, 25, 63, 39, 28, 29, 30, 31,
+	64, 79, 127, 123, 91, 108, 80, 125, 77, 93, 92, 78, 107, 96, 75, 97,
+	240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 122, 94, 76, 126, 110, 111,
+	124, 193, 194, 195, 196, 197, 198, 199, 200, 201, 209, 210, 211, 212, 213, 214,
+	215, 216, 217, 226, 227, 228, 229, 230, 231, 232, 233, 74, 224, 90, 95, 109,
+	121, 129, 130, 131, 132, 133, 134, 135, 136, 137, 145, 146, 147, 148, 149, 150,
+	151, 152, 153, 162, 163, 164, 165, 166, 167, 168, 169, 192, 106, 208, 161, 7,
+	32, 33, 34, 35, 36, 21, 6, 23, 40, 41, 42, 43, 44, 9, 10, 27,
+	48, 49, 26, 51, 52, 53, 54, 8, 56, 57, 58, 59, 4, 20, 62, 225,
+	65, 66, 67, 68, 69, 70, 71, 72, 73, 81, 82, 83, 84, 85, 86, 87,
+	88, 89, 98, 99, 100, 101, 102, 103, 104, 105, 112, 113, 114, 115, 116, 117,
+	118, 119, 120, 128, 138, 139, 140, 141, 142, 143, 144, 154, 155, 156, 157, 158,
+	159, 160, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183,
+	184, 185, 186, 187, 188, 189, 190, 191, 202, 203, 204, 205, 206, 207, 218, 219,
+	220, 221, 222, 223, 234, 235, 236, 237, 238, 239, 250, 251, 252, 253, 254, 255,
 }
 
-type DDM struct {
-	Length    int32
-	Magic     byte
-	Format    byte
-	CorrelId  int32
-	Length2   int32
-	CodePoint int32
+var ascii = [256]byte{
+	0, 1, 2, 3, 156, 9, 134, 127, 151, 141, 142, 11, 12, 13, 14, 15,
+	16, 17, 18, 19, 157, 133, 8, 135, 24, 25, 146, 143, 28, 29, 30, 31,
+	128, 129, 130, 131, 132, 10, 23, 27, 136, 137, 138, 139, 140, 5, 6, 7,
+	144, 145, 22, 147, 148, 149, 150, 4, 152, 153, 154, 155, 20, 21, 158, 26,
+	32, 160, 161, 162, 163, 164, 165, 166, 167, 168, 91, 46, 60, 40, 43, 33,
+	38, 169, 170, 171, 172, 173, 174, 175, 176, 177, 93, 36, 42, 41, 59, 94,
+	45, 47, 178, 179, 180, 181, 182, 183, 184, 185, 124, 44, 37, 95, 62, 63,
+	186, 187, 188, 189, 190, 191, 192, 193, 194, 96, 58, 35, 64, 39, 61, 34,
+	195, 97, 98, 99, 100, 101, 102, 103, 104, 105, 196, 197, 198, 199, 200, 201,
+	202, 106, 107, 108, 109, 110, 111, 112, 113, 114, 203, 204, 205, 206, 207, 208,
+	209, 126, 115, 116, 117, 118, 119, 120, 121, 122, 210, 211, 212, 213, 214, 215,
+	216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231,
+	123, 65, 66, 67, 68, 69, 70, 71, 72, 73, 232, 233, 234, 235, 236, 237,
+	125, 74, 75, 76, 77, 78, 79, 80, 81, 82, 238, 239, 240, 241, 242, 243,
+	92, 159, 83, 84, 85, 86, 87, 88, 89, 90, 244, 245, 246, 247, 248, 249,
+	48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 250, 251, 252, 253, 254, 255,
 }
 
-type Parameter struct {
-	Length    int32
-	CodePoint int32
-	Payload   []byte
-}
-
-func bytesToInt32(bytes ...byte) int32 {
-	if len(bytes) != 2 {
-		return 0
+func ToEBCDIC(data []byte) []byte {
+	var buf = bytes.NewBuffer(make([]byte, 0, len(data)))
+	for i := range data {
+		buf.WriteByte(ebcdic[data[i]])
 	}
-	return (int32(bytes[0]) << 8) | int32(bytes[1])
+	return buf.Bytes()
 }
 
-// readByte 读取单个 byte
-func readByte(br *bytes.Reader) byte {
-	b, _ := br.ReadByte()
-	return b
+func ToASCII(data []byte) []byte {
+	var buf = bytes.NewBuffer(make([]byte, 0, len(data)))
+	for i := range data {
+		buf.WriteByte(ascii[data[i]])
+	}
+	return buf.Bytes()
 }
 
-// readInt32 读取两个 byte
-func readInt32(br *bytes.Reader) int32 {
-	b1, _ := br.ReadByte()
-	b2, _ := br.ReadByte()
-	return (int32(b1) << 8) | int32(b2)
-}
+// func bytesToInt32(bytes ...byte) int32 {
+// 	if len(bytes) != 2 {
+// 		return 0
+// 	}
+// 	return (int32(bytes[0]) << 8) | int32(bytes[1])
+// }
 
-// readBytes 读取多个 byte
-func readBytes(br *bytes.Reader, length int) []byte {
-	var b = make([]byte, length)
-	br.Read(b)
-	return b
-}
+// // readByte 读取单个 byte
+// func readByte(br *bytes.Reader) byte {
+// 	b, _ := br.ReadByte()
+// 	return b
+// }
+
+// // readInt32 读取两个 byte
+// func readInt32(br *bytes.Reader) int32 {
+// 	b1, _ := br.ReadByte()
+// 	b2, _ := br.ReadByte()
+// 	return (int32(b1) << 8) | int32(b2)
+// }
+
+// // readBytes 读取多个 byte
+// func readBytes(br *bytes.Reader, length int) []byte {
+// 	var b = make([]byte, length)
+// 	br.Read(b)
+// 	return b
+// }
 
 // func ReadDrda(conn net.Conn) (DRDA, error) {
 // 	var b1 = make([]byte, 2)
